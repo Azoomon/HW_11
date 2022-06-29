2ed42a041267d67b7ec97d050fe6952f6b3da50f
Azumon <yakovlenkovadim@icloud.com>
2022-06-29 12:15:45 +0300

    Добавил докстринги, комментарии.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+"""Импортив фласк и все ранее прописанные функции из utils.py"""
 from flask import Flask, render_template
 from utils import get_candidates_by_skill, get_candidate, get_candidates_by_name, load_candidates_from_json
 
-
+"""Заводим представления для каждой странички (url)"""
 app = Flask(__name__)
 
 @app.route("/")
 def index_page():
+    """записываем все данные из джейсона в переменную, для работы с этими данными"""
     candidates = load_candidates_from_json('candidates.json')
+    """используем темплейт (шаблон html), указываем файл html, ссылаемся на candidates из функции чтобы была связь этих данных с html"""
     return render_template("list.html", candidates=candidates)
 
+"""Повторяем все с поправкой на требуемые нам данные и url"""
 @app.route("/candidate/<int:id>")
 def page_per_id(id):
     person = get_candidate(id)
@@ -25,5 +29,7 @@ def page_per_skill(skill_name):
     candidates = get_candidates_by_skill(skill_name)
     return render_template('skill.html', candidates=candidates, skill_name=skill_name)
 
+
+"""Предохранитель от случайного запуска"""
 if __name__ == "__main__":
     app.run()
--- utils.py
+++ utils.py
@@ -1,17 +1,18 @@
 import json
 
-
+"""Открываем джейсон на чтение"""
 def load_candidates_from_json(path):
     with open(path, "r", encoding="utf-8") as file:
         return json.load(file)
 
-
+"""Получаем кандидата по ID, ссылаясь по соответствующему ключу"""
 def get_candidate(candidate_id):
     candidates = load_candidates_from_json('candidates.json')
     for candidate in candidates:
         if candidate_id == candidate["id"]:
             return candidate
 
+"""Получаем список кандидатов по имени, аппендим кандидатов по совпадению в отдельный список"""
 def get_candidates_by_name(candidate_name):
     candidates = load_candidates_from_json('candidates.json')
     to_return = []
@@ -20,21 +21,22 @@ def get_candidates_by_name(candidate_name):
             to_return.append(candidate)
     return to_return
 
+"""Достаем кандидатов по скиллу, учитывая что поиск 'go' так же подпадает под 'djanGO' поэтому вначале
+ отчленяем из списка только склиллы, сплитим их в новом списке через запятую, а потом отрезаем через strip() ненужные символы по краям"""
 def get_candidates_by_skill(skill_name):
     candidates = load_candidates_from_json('candidates.json')
     approved = []
     for candidate in candidates:
-
+    #сложный вариант
     #     if skill_name in map(lambda skill: skill.lower().strip(), candidate['skills'].split(',')):
     #         approved.append(candidate)
     # return approved
-
 
         skills_str = candidate["skills"].lower()
         skills_list = skills_str.split(",")
         for index, skill in enumerate(skills_list):
             skills_list[index] = skill.strip()
 
-        if skill_name in skills_list:
+        if skill_name in skills_list: #аппендим список уже очищенными данными
             approved.append(candidate)
     return approved
